8af1b9fe2969755a01bc4d5bf7d0af4bbef210dc
LKc1024 <kelvinliv@sjtu.edu.cn>
2016-05-18 20:46:44 +0800

    Create newick.js

---
+++ newick.js
@@ -0,0 +1,39 @@
+var parseNewick = function(str) {
+    "use strict";
+    var ptr = 0;
+    function parseName() {
+	var name = "";
+	while (! ";,:)".includes(str[ptr])) {
+	    name += str[ptr++];
+	}
+	return name;
+    }
+    function parseLength() {
+	if (str[ptr] != ':') {
+	    return 1;
+	} else {
+	    ptr ++;
+	    var len = "";
+	    while (".1234567890".includes(str[ptr])) {
+		len += str[ptr++];
+	    }
+	    return (len.length > 0) ? parseFloat(len) : 1;
+	}
+    }
+    function parse() {
+	var node = {};
+	switch (str[ptr]) {
+	case '(':
+	    node.children = [];
+	    ptr ++;
+	    do {
+		node.children.push(parse());
+	    } while (str[ptr++] == ',')
+	default:
+	    node.name = parseName();
+	    node.data = { "branch_length" : parseLength() };
+	}
+	return node;
+    }
+    return parse();
+}
